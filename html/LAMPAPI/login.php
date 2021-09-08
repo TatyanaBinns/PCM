10fa36d8b72494b7864c8c1a648e759c1bba69da
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-09-08 16:42:15 -0400

    Add files via upload

--- html/LAMPAPI/login.php
+++ html/LAMPAPI/login.php
@@ -1,1 +1,58 @@
 
+<?php
+
+	$inData = getRequestInfo();
+
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
+
+	$conn = new mysqli("localhost", "contactsadmin", 'sdf1GFDG@$g2g2GSDhgfhDehsdgh4thFGSDFshdf', "pcm");
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT UserId,FirstName,LastName,PasswordHash FROM Users WHERE Email=?");
+		$stmt->bind_param("s", $inData["login"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if( $row = $result->fetch_assoc() and password_verify($inData["password"], $row['PasswordHash'])  )
+		{
+			returnWithInfo( $row['FirstName'], $row['LastName'], $row['UserId'] );
+		}
+		else
+		{
+			returnWithError("No Records Found");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>

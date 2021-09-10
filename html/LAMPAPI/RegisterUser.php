8c861ddcd33c36425c5b87d1c7437c6af14c1c17
bvanderzalm <70347711+bvanderzalm@users.noreply.github.com>
2021-09-10 16:21:14 -0400

    First commit for register user. Rough draft

---
+++ html/LAMPAPI/RegisterUser.php
@@ -0,0 +1,44 @@
+<?php
+	
+	$inData = getRequestInfo();
+
+	$firstName = $inData["firstname"];
+	$lastName = $inData["lastname"];
+	$phoneNumber = $inData["phonenumber"];
+	$email = $inData["email"];
+	$userId = $inData["userid"];
+	$password = $["password"];
+
+
+	$conn = new mysqli("localhost", "contactsadmin", "sdf1GFDG@$g2g2GSDhgfhDehsdgh4thFGSDFshdf", "pcm");
+	if ($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}
+
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, PhoneNumber, Email, UserId, PasswordHash) VALUES(?,?,?,?,?,?)");
+		$stmt->bind_param("ssssss", $firstName, $lastName, $phoneNumber, $email, $userId, password_hash($password, PASSWORD_DEFAULT));
+		$stmt->execute();
+		$stmt->close();
+		$stmt->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultsInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>

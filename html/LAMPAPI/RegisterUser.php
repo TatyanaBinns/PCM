8dc9590ae525825f9c9d20f8385716e2b4e96a9b
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-09-11 13:11:00 -0400

    Update RegisterUser.php

--- html/LAMPAPI/RegisterUser.php
+++ html/LAMPAPI/RegisterUser.php
@@ -1,15 +1,15 @@
 <?php
-	
+
 	$inData = getRequestInfo();
 
+	$userId = $inData["userId"];
 	$firstName = $inData["firstname"];
 	$lastName = $inData["lastname"];
 	$email = $inData["email"];
-	$userId = $inData["userid"];
-	$password = $["password"];
+	$password = $inData["password"];
 
 
-	$conn = new mysqli("localhost", "contactsadmin", "sdf1GFDG@$g2g2GSDhgfhDehsdgh4thFGSDFshdf", "pcm");
+	$conn = new mysqli("localhost", "contactsadmin", 'sdf1GFDG@$g2g2GSDhgfhDehsdgh4thFGSDFshdf', "pcm");
 	if ($conn->connect_error)
 	{
 		returnWithError($conn->connect_error);
@@ -17,12 +17,27 @@
 
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Users (FirstName, LastName, Email, UserId, PasswordHash) VALUES(?,?,?,?,?)");
-		$stmt->bind_param("sssss", $firstName, $lastName, $email, $userId, password_hash($password, PASSWORD_DEFAULT));
+		$stmt = $conn->prepare("SELECT FirstName, LastName, Email FROM Users WHERE (FirstName=? AND LastName=?) OR Email=?");
+		$stmt->bind_param("sss", $firstName, $lastName, $email);
 		$stmt->execute();
-		$stmt->close();
-		$stmt->close();
-		returnWithError("");
+		$result = $stmt->get_result();
+
+		if( $row = $result->fetch_assoc() )
+		{
+			returnWithError("User already exists.");
+			$stmt->close();
+		}
+		else
+		{
+			$stmt->close();
+
+			$stmt = $conn->prepare("INSERT into Users (UserId, FirstName, LastName, Email, PasswordHash) VALUES(?,?,?,?,?)");
+			$stmt->bind_param("issss", $userId, $firstName, $lastName, $email, password_hash($password, PASSWORD_DEFAULT));
+			$stmt->execute();
+			$stmt->close();
+			$conn->close();
+			returnWithError("");
+		}
 	}
 
 	function getRequestInfo()
@@ -30,9 +45,10 @@
 		return json_decode(file_get_contents('php://input'), true);
 	}
 
-	function sendResultsInfoAsJson($obj)
+	function sendResultInfoAsJson($obj)
 	{
 		header('Content-type: application/json');
+		echo $obj;
 	}
 
 	function returnWithError( $err )

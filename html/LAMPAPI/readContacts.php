b002bc8b7582c6b9e5e2c335858011085aac7ac4
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-09-25 20:30:54 -0400

    Create readContacts.php

---
+++ html/LAMPAPI/readContacts.php
@@ -0,0 +1,67 @@
+<?php
+
+	$inData = getRequestInfo();
+
+	$readResults = "";
+	$readCount = 0;
+
+	$conn = new mysqli("localhost", "contactsadmin", 'sdf1GFDG@$g2g2GSDhgfhDehsdgh4thFGSDFshdf', "pcm");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT ContactsId, NameFirst, NameLast, Email, PhoneNumber, DateCreated FROM Contacts WHERE UserId = ?");
+		$stmt->bind_param("i", $inData["userId"]);
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '"' . $row["ContactsId"] . " : " . $row["NameFirst"] . " : " . $row["NameLast"] . " : " . $row["Email"] . " : " . $row["PhoneNumber"] . " : " . $row["DateCreated"] . '"';
+		}
+
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Records Found" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>

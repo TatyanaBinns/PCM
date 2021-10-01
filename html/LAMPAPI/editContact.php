976fd02d42e0a964cfda53e35a5bf4358bf22fa3
kat0309 <89745552+kat0309@users.noreply.github.com>
2021-09-30 21:29:33 -0400

    Update editContact.php

--- html/LAMPAPI/editContact.php
+++ html/LAMPAPI/editContact.php
@@ -16,19 +16,32 @@
 
 	else
 	{
-		$stmt = $conn->prepare("UPDATE Contacts SET NameFirst=?, NameLast=?, Email=?, PhoneNumber=? WHERE UserId=? AND ContactsId=?");
-		$stmt->bind_param("ssssii", $firstname, $lastname, $email, $phonenum, $userId, $contactId);
-		$stmt->execute();
-
-		if ($conn->affected_rows > 0)
+		if (strlen($phonenum) != 10 OR strpos($phonenum, "(") !== false OR strpos($phonenum, ")") !== false OR strpos($phonenum, "-") !== false)
 		{
-			returnWithError("");
+			returnWithError("Phone number format is not correct");
 		}
 		else {
-			returnWithError("Contact doesn't exist.");
+			$stmt = $conn->prepare("SELECT UserId, Email, ContactsId FROM Contacts WHERE UserId=? AND Email=?");
+			$stmt->bind_param("is", $userId, $email);
+			$stmt->execute();
+			$result = $stmt->get_result();
+
+			if( $row = $result->fetch_assoc() AND $row["ContactsId"] != $contactId )
+			{
+				returnWithError("Email already in use");
+				$stmt->close();
+			}
+			else {
+				$stmt->close();
+				$stmt = $conn->prepare("UPDATE Contacts SET NameFirst=?, NameLast=?, Email=?, PhoneNumber=? WHERE UserId=? AND ContactsId=?");
+				$stmt->bind_param("ssssii", $firstname, $lastname, $email, $phonenum, $userId, $contactId);
+				$stmt->execute();
+
+				returnWithError("");
+				$stmt->close();
+			}
+			$conn->close();
 		}
-		$stmt->close();
-		$conn->close();
 	}
 
 	function getRequestInfo()
